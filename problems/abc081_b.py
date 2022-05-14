511ed27e10cd371b5655a4f389ee65eaf13d262d
Nag729 <nagamaaaaachi@gmail.com>
2022-05-15 01:31:38 +0900

    fix: indent size

--- problems/abc081_b.py
+++ problems/abc081_b.py
@@ -5,7 +5,7 @@ denominator = 2
 count = 0
 
 while(all(a % denominator == 0 for a in A_list)):
-    count += 1
-    denominator *= 2
+  count += 1
+  denominator *= 2
 
 print(count)
--- problems/abc087_b.py
+++ problems/abc087_b.py
@@ -6,9 +6,9 @@ X = int(input())  # total
 count = 0
 
 for i in range(A + 1):
-    for j in range(B + 1):
-        for k in range(C + 1):
-            if 500 * i + 100 * j + 50 * k == X:
-                count += 1
+  for j in range(B + 1):
+    for k in range(C + 1):
+      if 500 * i + 100 * j + 50 * k == X:
+        count += 1
 
 print(count)
